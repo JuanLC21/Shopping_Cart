0cb819f7e2d2cb008ad04b2bdf658bb3d63f0d10
Juan Sebastian <jsebastianlc@gmail.com>
2024-09-21 23:01:28 -0500

    adjust cors server

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router
     .use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); 
+        res.header('Access-Control-Allow-Origin', 'https://shopping-cart-client-7mr1.onrender.com/'); 
         res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE'); 
         res.header('Access-Control-Allow-Headers', 'Content-Type'); 
         next();
