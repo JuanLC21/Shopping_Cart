3957415121f8688e4832321e1de1185680308aa1
Juan Sebastian <jsebastianlc@gmail.com>
2024-09-30 13:23:31 -0500

    test radom

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router
     .use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', SERVER_URL); 
+        res.header('Access-Control-Allow-Origin', 'https://client-shopping-cart.onrender.com'); 
         res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE'); 
         res.header('Access-Control-Allow-Headers', 'Content-Type'); 
         next();
