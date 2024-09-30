3eabdd6f8ad200723dc1c19d54fe535d7d35a46f
Juan Sebastian <jsebastianlc@gmail.com>
2024-09-30 00:29:35 -0500

    require path turn into import path

--- server/src/data/products.js
+++ server/src/data/products.js
@@ -1,5 +1,9 @@
+import path from 'path';
 import { promises as fs } from 'fs'
-import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const PATH = path.join(__dirname, 'data/dataPlatzi.json')
 
