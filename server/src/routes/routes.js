3687cfc2959c098ea0eac848ac7bdc1071a66f7c
Juan Sebastian <jsebastianlc@gmail.com>
2024-09-30 20:41:55 -0500

    some url's names has been changed

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { getAll, getProduct, putProduct, postProduct, deleteProduct } from '../controllers/controllers.js'
-import { SERVER_URL } from '../config.js' 
+import { CLIENT_URL } from '../config.js' 
 
 const router = express.Router()
 
 router
     .use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', 'https://client-shopping-cart.onrender.com'); 
+        res.header('Access-Control-Allow-Origin', CLIENT_URL); 
         res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE'); 
         res.header('Access-Control-Allow-Headers', 'Content-Type'); 
         next();
