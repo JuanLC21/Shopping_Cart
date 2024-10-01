bb70aff8ecef4179e0a93f2a9bd56124aa5e83e1
Juan Sebastian <jsebastianlc@gmail.com>
2024-09-30 22:50:48 -0500

    update again

--- server/src/config.js
+++ server/src/config.js
@@ -1,3 +1,3 @@
-// Enviroment Variable
+// Enviroment Variable Frontend
 export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173'
 export const SERVER_PORT = process.env.SERVER_PORT || 3000
